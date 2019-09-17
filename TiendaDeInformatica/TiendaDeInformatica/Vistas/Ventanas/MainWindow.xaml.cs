0eff6f50795bdef10ec7a364d2fd75679327d7b4
Federico Desía <fdesia@eest1chivilcoy.edu.ar>
2019-09-17 20:46:07 -0300

    - Diseño básico de la ventana

--- TiendaDeInformatica/TiendaDeInformatica/Vistas/Ventanas/MainWindow.xaml.cs
+++ TiendaDeInformatica/TiendaDeInformatica/Vistas/Ventanas/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace TiendaDeInformatica
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
