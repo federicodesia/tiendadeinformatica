d93e1a1087d0f8e3720075c80089deba00f576be
Federico De Sía y Facundo Aguel <fdesia@eest1chivilcoy.edu.ar>
2019-09-23 18:20:45 -0300

    - Comenzamos a crear los modelos

---
+++ TiendaDeInformatica/TiendaDeInformatica/Modelos/Presupuesto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaDeInformatica.Modelos
+{
+    public class Presupuesto
+    {
+        public int Id { get; set; }
+        public Cliente Cliente { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public decimal PrecioTotal { get; set; }
+        public List<> Productos { get; set; }
+    }
+}
---
+++ TiendaDeInformatica/TiendaDeInformatica/Modelos/Productos/Procesador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaDeInformatica.Modelos.Productos
+{
+    public class Procesador : Producto
+    {
+        public decimal PrecioTotal { get; set; }
+
+        public Procesador()
+        {
+            this.
+        }
+    }
+}
