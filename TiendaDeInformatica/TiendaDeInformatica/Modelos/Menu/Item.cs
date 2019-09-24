ef8f0cfa68367fb84485cf86335187747b3dad73
Federico De Sía y Facundo Aguel <fdesia@eest1chivilcoy.edu.ar>
2019-09-24 11:56:35 -0300

    - Estructura básica del menú. * Faltan agregar los iconos de cada Item.

---
+++ TiendaDeInformatica/TiendaDeInformatica/Modelos/Menu/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TiendaDeInformatica.Modelos.Menu
+{
+    public class Item
+    {
+        public Item(string header, List<SubItem> subItems, Image icon)
+        {
+            Header = header;
+            SubItems = subItems;
+            Icon = icon;
+        }
+
+        public Item(string header, UserControl screen, Image icon)
+        {
+            Header = header;
+            Screen = screen;
+            Icon = icon;
+        }
+
+        public string Header { get; private set; }
+        public Image Icon { get; private set; }
+        public List<SubItem> SubItems { get; private set; }
+        public UserControl Screen { get; private set; }
+    }
+}
--- TiendaDeInformatica/TiendaDeInformatica/Vistas/Ventanas/MainWindow.xaml.cs
+++ TiendaDeInformatica/TiendaDeInformatica/Vistas/Ventanas/MainWindow.xaml.cs
@@ -13,17 +13,46 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TiendaDeInformatica.Modelos.Menu;
+using TiendaDeInformatica.Vistas.Controles_de_Usuario;
 
 namespace TiendaDeInformatica
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            var PresupuestosSubItems = new List<SubItem>();
+            var Presupuestos = new Item("Presupuestos", PresupuestosSubItems, null);
+            Menu.Children.Add(new ItemUserControl(Presupuestos));
+
+            var ArmadoDePCSubItems = new List<SubItem>();
+            ArmadoDePCSubItems.Add(new SubItem("Procesadores"));
+            ArmadoDePCSubItems.Add(new SubItem("Placas Madre"));
+            ArmadoDePCSubItems.Add(new SubItem("Memorias RAM"));
+            ArmadoDePCSubItems.Add(new SubItem("Placas de Video"));
+            ArmadoDePCSubItems.Add(new SubItem("Almacenamiento"));
+            ArmadoDePCSubItems.Add(new SubItem("Fuentes"));
+            ArmadoDePCSubItems.Add(new SubItem("Gabinetes"));
+            var ArmadoDePC = new Item("Armado de PC", ArmadoDePCSubItems, null);
+            Menu.Children.Add(new ItemUserControl(ArmadoDePC));
+
+            var PerifericosSubItems = new List<SubItem>();
+            PerifericosSubItems.Add(new SubItem("Teclados"));
+            PerifericosSubItems.Add(new SubItem("Mouses"));
+            PerifericosSubItems.Add(new SubItem("Mousepads"));
+            PerifericosSubItems.Add(new SubItem("Monitores"));
+            PerifericosSubItems.Add(new SubItem("Parlantes"));
+            var Productos = new Item("Periféricos", PerifericosSubItems, null);
+            Menu.Children.Add(new ItemUserControl(Productos));
+
+            var OtrosProductosSubItems = new List<SubItem>();
+            OtrosProductosSubItems.Add(new SubItem("Pendrives"));
+            OtrosProductosSubItems.Add(new SubItem("Discos externos"));
+            var OtrosProductos = new Item("Otros productos", OtrosProductosSubItems, null);
+            Menu.Children.Add(new ItemUserControl(OtrosProductos));
         }
     }
 }
